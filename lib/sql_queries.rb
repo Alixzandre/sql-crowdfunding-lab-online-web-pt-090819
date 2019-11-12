b8ed57acf6a86f6a8f2c75ef646d4c7296cfafa7
Alexander Bispo <alexbispo173@gmail.com>
2019-11-12 22:04:30 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,9 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   SELECT users.name, users.age, SUM(pledges.amount)
-  FROM projects
+  FROM users
+  INNER JOIN pledges
+  
   
   
 end
