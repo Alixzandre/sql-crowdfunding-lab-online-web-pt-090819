97c9806b4b387bb1cdc9774b25ae8f5005cb8e84
Alexander Bispo <alexbispo173@gmail.com>
2019-11-12 22:19:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,7 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   SELECT projects.category, pledges.amount
-  FROM users
+  FROM projects
   JOIN pledges ON 
 end
 
